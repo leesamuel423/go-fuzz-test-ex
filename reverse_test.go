563732bf5c22f5c098266b4ed50b1f3afa25388e
Samuel Lee <leesamuel423@gmail.com>
2025-04-13 00:15:46 -0400

    testing: address edge cases utf8 + runes

--- reverse_test.go
+++ reverse_test.go
@@ -2,20 +2,46 @@ package main
 
 import (
 	"testing"
+	"unicode/utf8"
 )
 
-func TestReverse(t *testing.T) {
-	testcases := []struct {
-		input, want string
-	}{
-		{"Hello, world", "dlrow ,olleH"},
-		{" ", " "},
-		{"!12345", "54321!"},
-	}
+func FuzzReverse(f *testing.F) {
+	testcases := []string{"Hello, world", " ", "!12345"}
 	for _, tc := range testcases {
-		rev := Reverse(tc.input)
-		if rev != tc.want {
-			t.Errorf("Reverse: %q, want %q", rev, tc.want)
-		}
+		// seed corpus → data to initialize and improve testing
+		// helps fuzzer understand structure and format of valid inputs
+		f.Add(tc)
 	}
+	f.Fuzz(func(t *testing.T, orig string) {
+		rev, revErr := Reverse(orig)
+		if revErr != nil {
+			return
+		}
+		doubleRev, doubleRevErr := Reverse(rev)
+		if doubleRevErr != nil {
+			return
+		}
+		if orig != doubleRev {
+			t.Errorf("Before: %q, after: %q", orig, doubleRev)
+		}
+		if utf8.ValidString(orig) && !utf8.ValidString(rev) {
+			t.Errorf("Reverse produced invalid UTF-8 string %q", rev)
+		}
+	})
 }
+
+// func TestReverse(t *testing.T) {
+// 	testcases := []struct {
+// 		input, want string
+// 	}{
+// 		{"Hello, world", "dlrow ,olleH"},
+// 		{" ", " "},
+// 		{"!12345", "54321!"},
+// 	}
+// 	for _, tc := range testcases {
+// 		rev := Reverse(tc.input)
+// 		if rev != tc.want {
+// 			t.Errorf("Reverse: %q, want %q", rev, tc.want)
+// 		}
+// 	}
+// }
